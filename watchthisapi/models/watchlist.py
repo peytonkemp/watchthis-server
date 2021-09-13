7ef6e4cbaf47589adf48af6375d5244e3f866d02
peytonkemp <peyton.kemp@gmail.com>
2021-09-13 18:15:06 -0500

    moviegenres and tvgenres fixtures

--- watchthisapi/models/watchlist.py
+++ watchthisapi/models/watchlist.py
@@ -8,6 +8,6 @@ class Watchlist(models.Model):
     """
     name = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    
     def __str__(self):
         return self.name
