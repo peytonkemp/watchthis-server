a023a22a12099ccd60cc6d9e910e52bda8d432d9
peytonkemp <peyton.kemp@gmail.com>
2021-09-13 14:37:03 -0500

    watchlist.py User fix

--- watchthisapi/models/watchlist.py
+++ watchthisapi/models/watchlist.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Watchlist(models.Model):
     """Genre model
@@ -6,7 +7,7 @@ class Watchlist(models.Model):
         name (CharField): name of the game type
     """
     name = models.CharField(max_length=50)
-    user_id = models.ForeignKey("User", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
