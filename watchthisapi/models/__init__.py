2003efd40d1d89fcb73a542009cf0907a1ae667a
peytonkemp <peyton.kemp@gmail.com>
2021-09-13 14:17:41 -0500

    added tv_watchlist and movie_watchlist to __init__.py

--- watchthisapi/models/__init__.py
+++ watchthisapi/models/__init__.py
@@ -4,3 +4,5 @@ from .movie_genre import MovieGenre
 from .tv import Tv
 from .tv_genre import TvGenre
 from .watchlist import Watchlist
+from .tv_watchlist import TvWatchlist
+from .movie_watchlist import MovieWatchlist
